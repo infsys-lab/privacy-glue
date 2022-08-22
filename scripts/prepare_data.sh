2d670db509e53326286fe0e2e2cb250a4eb4fe3e
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-22 19:12:27 +0200

    Remove `opt_out` function from `prepare_data.sh`

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -42,34 +42,6 @@ app_350() {
   rm -rf "$target/APP-350_v1.1"
 }
 
-opt_out() {
-  local target="./data/opt_out" query
-  query=$(
-    cat <<EOF
-SELECT url, full_sentence_text, hyperlink_text, label
-FROM hyperlinks
-WHERE label IN ("Positive", "Negative");
-EOF
-  )
-  mkdir -p "$target"
-  wget -N -P "$target" \
-    "https://usableprivacy.org/static/data/OptOutChoice-2020_v1.0.zip"
-  unzip -o "$target/OptOutChoice-2020_v1.0.zip" -d "$target"
-  rsync -a "$target/OptOutChoice-2020_v1.0/" "$target"
-  rm -rf "$target/OptOutChoice-2020_v1.0/"
-  sqlite3 -json "$target/binary_data/policies.db" "$query" \
-    >"$target/binary_data/binary_data.json"
-  cat <<<"$(jq -c '.[]' "$target/binary_data/binary_data.json")" \
-  >"$target/binary_data/binary_data.json"
-  cat "$target/category_data/train_set1.jsonl" \
-    "$target/category_data/train_set2.jsonl" \
-    "$target/category_data/train_set3.jsonl" \
-    >"$target/category_data/train_set.jsonl"
-  rm -f "$target/category_data/train_set1.jsonl" \
-    "$target/category_data/train_set2.jsonl" \
-    "$target/category_data/train_set3.jsonl"
-}
-
 clean() {
   find data -name ".DS_Store" -or -name ".idea" -or -name "__MACOSX" |
     xargs -n 1 rm -rf
@@ -79,7 +51,6 @@ clean() {
 main() {
   policy_detection
   app_350
-  opt_out
   clean
 }
 
