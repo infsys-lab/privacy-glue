a97ccfef7efac686614a91283b00f9e5f88b6e13
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-23 15:36:02 +0200

    Update `src/tasks/policy_detection.py`
    1. Add comments to each block for general code understanding
    2. Replace column boolean labels with strings

--- src/tasks/policy_detection.py
+++ src/tasks/policy_detection.py
@@ -7,9 +7,23 @@ import os
 
 
 def load_policy_detection(directory: str) -> datasets.DatasetDict:
+    # read csv file and choose subset of columns
     df = pd.read_csv(os.path.join(directory, "1301_dataset.csv"), index_col=0)
     df = df[["policy_text", "is_policy"]]
-    df["is_policy"] = df["is_policy"].astype(int)
+
+    # replace labels from boolean to strings for consistency
+    df["is_policy"] = df["is_policy"].replace({
+        True: "Policy",
+        False: "Not Policy"
+    })
+
+    # rename columns for consistency
     df = df.rename(columns={"policy_text": "text", "is_policy": "label"})
+
+    # convert into HF datasets
     dataset = datasets.Dataset.from_pandas(df, preserve_index=False)
-    return dataset.train_test_split(test_size=0.3, seed=42)
+
+    # split into train and test sets
+    dataset = dataset.train_test_split(test_size=0.3, seed=42)
+
+    return dataset
