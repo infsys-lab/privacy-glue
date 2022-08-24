7c752b1afd1d8622a5bdf164a863e4e2c1a9a00e
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-24 11:28:15 +0200

    Update `scripts/prepare_data.sh`
    Modify `mv` command to `cp -rf` to make script idempotent. Otherwise, if
    this is script is executed once again, it exits with an error message
    "Directory not empty" reflecting the inability of `mv` to overwrite
    directories

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -38,7 +38,7 @@ app_350() {
   mkdir -p "$target"
   wget -N -P "$target" "https://usableprivacy.org/static/data/APP-350_v1.1.zip"
   unzip -o "$target/APP-350_v1.1.zip" -d "$target"
-  mv "$target/APP-350_v1.1/"* "$target"
+  cp -rf "$target/APP-350_v1.1/"* "$target"
   rm -rf "$target/APP-350_v1.1"
   rm -rf "$target/__MACOSX"
 }
