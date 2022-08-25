39e8d37978632849064ba245f6d53b5280263a16
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-25 17:28:41 +0200

    Update `src/privacy_glue.py`
    1. Update new split PolicyIE tasks
    2. Spellfix PolicyQA task

--- src/privacy_glue.py
+++ src/privacy_glue.py
@@ -10,7 +10,8 @@ from tasks.app_350 import load_app_350
 from tasks.opp_115 import load_opp_115
 from tasks.piextract import load_piextract
 from tasks.policy_detection import load_policy_detection
-from tasks.policy_ie import load_policy_ie_a, load_policy_ie_b
+from tasks.policy_ie_a import load_policy_ie_a
+from tasks.policy_ie_b import load_policy_ie_b
 from tasks.policy_qa import load_policy_qa
 from tasks.privacy_qa import load_privacy_qa
 import transformers
@@ -109,9 +110,7 @@ def train(model_args: ModelArguments, data_args: DataArguments,
         checkpoint = None
 
     # define new argument based on task name
-    data_args.task_dir = os.path.join(
-        data_args.data_dir, data_args.task) if not data_args.task.startswith(
-            "policy_ie") else os.path.join(data_args.data_dir, "policy_ie")
+    data_args.task_dir = os.path.join(data_args.data_dir, data_args.task)
 
     # load dataset based on task name
     if data_args.task == "app_350":
@@ -126,7 +125,7 @@ def train(model_args: ModelArguments, data_args: DataArguments,
         data = load_policy_ie_a(data_args.task_dir)
     elif data_args.task == "policy_ie_b":
         data = load_policy_ie_b(data_args.task_dir)
-    elif data_args.task == "pblicy_qa":
+    elif data_args.task == "policy_qa":
         data = load_policy_qa(data_args.task_dir)
     elif data_args.task == "privacy_qa":
         data = load_privacy_qa(data_args.task_dir)
