c326fe7459445c672618b6d69124a3dbda390ac6
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-26 17:42:17 +0200

    Add test case for `load_policy_detection`

---
+++ tests/test_load_policy_detection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tasks.policy_detection import load_policy_detection
+import os
+
+
+def test_load_policy_ie_a():
+    # load sample data
+    data = load_policy_detection(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "data",
+                     "policy_detection"))
+
+    # check that all three splits are included
+    assert set(data.keys()) == {"train", "test"}
+
+    # iterate over splits
+    for (split, data_split) in data.items():
+        # check that all column names are as expected
+        assert data_split.column_names == ["text", "label"]
+
+        # ensure all text is composed of strings and not array-like objects
+        assert all([isinstance(text, str) for text in data_split["text"]])
+
+        # ensure all labels are composed of strings and not array-like objects
+        assert all([isinstance(label, str) for label in data_split["label"]])
