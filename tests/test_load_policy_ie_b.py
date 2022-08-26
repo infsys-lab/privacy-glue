e6e186bb03233f504d4b3ddd3a61a119a91a0b9b
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-08-26 13:01:35 +0200

    Update `tests/test_load_policy_ie_b.py`
    Add more constraints for checking

--- tests/test_load_policy_ie_b.py
+++ tests/test_load_policy_ie_b.py
@@ -19,10 +19,33 @@ def test_load_policy_ie_b():
         # check that all column names are as expected
         assert data_split.column_names == ["tokens", "ner_tags"]
 
-        # check that tokens and NER tags have the same length
-        assert len(data_split["tokens"]) == len(data_split["ner_tags"])
+        # check that all tokens are present in list objects
+        assert all([
+            isinstance(tokens_sample, list)
+            for tokens_sample in data_split["tokens"]
+        ])
+
+        # check that all tokens are present as strings
+        assert all([
+            isinstance(token, str) for tokens_sample in data_split["tokens"]
+            for token in tokens_sample
+        ])
+
+        # check that all NER tags are present in list objects
+        assert all([
+            isinstance(ner_tags_sample, list)
+            for ner_tags_sample in data_split["ner_tags"]
+        ])
 
         # check that all NER tags have two elements
-        for ner_tag_sample in data_split["ner_tags"]:
-            for ner_tag in ner_tag_sample:
-                assert len(ner_tag) == 2
+        assert all([
+            len(ner_tag) == 2 and all([isinstance(x, str) for x in ner_tag])
+            for ner_tag_sample in data_split["ner_tags"]
+            for ner_tag in ner_tag_sample
+        ])
+
+        # check that tokens and NER tags have the same length
+        assert all([
+            len(tokens_sample) == len(ner_tag_sample) for tokens_sample,
+            ner_tag_sample in zip(data_split["tokens"], data_split["ner_tags"])
+        ])
