bdad3d50bbd4e9fc7ddd9a0286f46bb63d98c788
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-09-05 15:37:43 +0200

    Add new test for `logging_utils.py`

---
+++ tests/utils/test_logging_utils.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from utils.logging_utils import (
+    init_logger,
+    add_file_handler,
+    remove_all_file_handlers,
+    FORMATTER,
+)
+import logging
+import pytest
+import tempfile
+import os
+
+
+@pytest.mark.parametrize(
+    "level",
+    ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+)
+def test_init_logger(level):
+    logger = logging.getLogger(level)
+    init_logger(logger, level)
+    assert logger.level == getattr(logging, level)
+    assert isinstance(logger.handlers[-1], logging.StreamHandler)
+    assert logger.handlers[-1].level == getattr(logging, level)
+    assert logger.handlers[-1].formatter == FORMATTER
+
+
+@pytest.mark.parametrize(
+    "level, filename",
+    [
+        ("DEBUG", "debug.txt"),
+        ("INFO", "info.txt"),
+        ("WARNING", "warning.txt"),
+        ("ERROR", "error.txt"),
+        ("CRITICAL", "critical.txt"),
+    ],
+)
+def test_add_file_handler(level, filename):
+    logger = logging.getLogger(level)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = os.path.join(tmp_dir, filename)
+        add_file_handler(logger, level, path)
+        assert isinstance(logger.handlers[-1], logging.FileHandler)
+        assert logger.handlers[-1].level == getattr(logging, level)
+        assert logger.handlers[-1].formatter == FORMATTER
+        assert logger.handlers[-1].baseFilename == path
+
+
+@pytest.mark.parametrize(
+    "level, filename",
+    [
+        ("DEBUG", "debug.txt"),
+        ("INFO", "info.txt"),
+        ("WARNING", "warning.txt"),
+        ("ERROR", "error.txt"),
+        ("CRITICAL", "critical.txt"),
+    ],
+)
+def test_remove_all_file_handlers(level, filename):
+    logger = logging.getLogger(level)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = os.path.join(tmp_dir, filename)
+        add_file_handler(logger, level, path)
+        remove_all_file_handlers(logger)
+        assert not any(
+            [isinstance(handler, logging.FileHandler) for handler in logger.handlers]
+        )
