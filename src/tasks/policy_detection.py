0386e305f789b4cae66742fc59e77d89b38b608f
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-09-06 11:06:08 +0200

    Update `src/tasks/policy_detection.py`
    1. Add workflow to read split IDs from JSON file
    2. Add workflow to filter via indices from pandas
    3. Add assertion to ensure that data and JSON lengths are the same (in
       case of length modifications upstream)

--- src/tasks/policy_detection.py
+++ src/tasks/policy_detection.py
@@ -3,13 +3,24 @@
 
 import pandas as pd
 import datasets
+import json
 import os
 
 
 def load_policy_detection(directory: str) -> datasets.DatasetDict:
+    # initialize DatasetDict object
+    combined = datasets.DatasetDict()
+
     # read csv file and choose subset of columns
     df = pd.read_csv(os.path.join(directory, "1301_dataset.csv"), index_col=0)
     df = df[["policy_text", "is_policy"]]
+
+    # read json file with split ids
+    with open(os.path.join(directory, "split_ids.json"), "r") as input_file_stream:
+        split_ids = json.load(input_file_stream)
+
+    # assert that all lengths make sense
+    assert sum([len(ids) for ids in split_ids.values()]) == df.shape[0]
 
     # replace labels from boolean to strings for consistency
     df["is_policy"] = df["is_policy"].replace({True: "Policy", False: "Not Policy"})
@@ -18,9 +29,12 @@ def load_policy_detection(directory: str) -> datasets.DatasetDict:
     df = df.rename(columns={"policy_text": "text", "is_policy": "label"})
 
     # convert into HF datasets
-    dataset = datasets.Dataset.from_pandas(df, preserve_index=False)
+    dataset = datasets.Dataset.from_pandas(df, preserve_index=True)
 
     # split into train and test sets
-    dataset = dataset.train_test_split(test_size=0.3, seed=42)
+    for split, ids in split_ids.items():
+        combined[split] = dataset.filter(
+            lambda example: example["__index_level_0__"] in ids
+        ).remove_columns("__index_level_0__")
 
-    return dataset
+    return combined
