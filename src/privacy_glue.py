aa21878a43af9fe5f91b9773d52fb632619a3a3a
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-09-07 08:37:44 +0200

    Bugfix `privacy_glue.py` and modify structure
    1. Bugfix `TASKS` containing `all` which is incorrect
    2. Re-order `train` such that script branches into various task types
    3. Add `try-finally` block to allow removing file handlers at the end
       and to save a success ful
    4. Rename exit code file to success file (more meaningful in this
       context)

--- src/privacy_glue.py
+++ src/privacy_glue.py
@@ -9,13 +9,6 @@ from utils.logging_utils import (
     remove_all_file_handlers,
 )
 from parser import TASKS, ModelArguments, DataArguments, get_parser
-from tasks.opp_115 import load_opp_115
-from tasks.piextract import load_piextract
-from tasks.policy_detection import load_policy_detection
-from tasks.policy_ie_a import load_policy_ie_a
-from tasks.policy_ie_b import load_policy_ie_b
-from tasks.policy_qa import load_policy_qa
-from tasks.privacy_qa import load_privacy_qa
 import transformers
 import datasets
 import logging
@@ -27,11 +20,11 @@ import re
 LOGGER = logging.getLogger(__name__)
 
 # define exit code file
-EXIT_CODE_FILE = "exit_code"
+SUCCESS_FILE = ".success"
 
 
-def save_exit_code(filename: str, code: int = 0) -> None:
-    with open(filename, "w") as output_file_stream:
+def save_success_file(directory: str, code: int = 0) -> None:
+    with open(os.path.join(directory, SUCCESS_FILE), "w") as output_file_stream:
         output_file_stream.write("%s\n" % code)
 
 
@@ -44,113 +37,109 @@ def train(
     data_args: DataArguments,
     training_args: TrainingArguments,
 ) -> None:
-    # create output_dir if it does not exit
-    os.makedirs(training_args.output_dir, exist_ok=True)
+    try:
+        # create output_dir if it does not exit
+        os.makedirs(training_args.output_dir, exist_ok=True)
 
-    # configure local logger
-    global LOGGER
-    remove_all_file_handlers(LOGGER)
-    add_file_handler(
-        LOGGER,
-        training_args.get_process_log_level(),
-        os.path.join(training_args.output_dir, "session.log"),
-    )
-
-    # configure datasets logger
-    # NOTE: if working with distributed training, logging/saving would only
-    # need to be configured on the main or zero process
-    datasets.utils.logging.set_verbosity(training_args.get_process_log_level())
-    remove_all_file_handlers(datasets.utils.logging.get_logger())
-    add_file_handler(
-        datasets.utils.logging.get_logger(),
-        training_args.get_process_log_level(),
-        os.path.join(training_args.output_dir, "session.log"),
-    )
-
-    # configure transformers logger
-    # NOTE: if working with distributed training, logging/saving would only
-    # need to be configured on the main or zero process
-    transformers.utils.logging.set_verbosity(training_args.get_process_log_level())
-    transformers.utils.logging.enable_default_handler()
-    transformers.utils.logging.enable_explicit_format()
-    remove_all_file_handlers(transformers.utils.logging.get_logger())
-    add_file_handler(
-        transformers.utils.logging.get_logger(),
-        training_args.get_process_log_level(),
-        os.path.join(training_args.output_dir, "session.log"),
-    )
-
-    # check for existing exit code and decide action
-    if (
-        os.path.exists(os.path.join(training_args.output_dir, EXIT_CODE_FILE))
-        and not training_args.overwrite_output_dir
-        and training_args.do_train
-    ):
-        LOGGER.info("Exit-code 0: training already complete, exiting")
-        return None
-
-    # dump miscellaneous arguments
-    torch.save(
-        {"model_args": model_args, "data_args": data_args},
-        os.path.join(training_args.output_dir, "misc_args.bin"),
-    )
-
-    # log on each process the small summary
-    LOGGER.info(
-        (
-            "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, "
-            "16-bits training: %s"
+        # configure local logger
+        global LOGGER
+        add_file_handler(
+            LOGGER,
+            training_args.get_process_log_level(),
+            os.path.join(training_args.output_dir, "session.log"),
         )
-        % (
-            training_args.local_rank,
-            training_args.device,
-            training_args.n_gpu,
-            bool(training_args.local_rank != -1),
-            training_args.fp16,
+
+        # configure datasets logger
+        # NOTE: if working with distributed training, logging/saving would only
+        # need to be configured on the main or zero process
+        datasets.utils.logging.set_verbosity(training_args.get_process_log_level())
+        remove_all_file_handlers(datasets.utils.logging.get_logger())
+        add_file_handler(
+            datasets.utils.logging.get_logger(),
+            training_args.get_process_log_level(),
+            os.path.join(training_args.output_dir, "session.log"),
         )
-    )
 
-    # set seed before initializing model
-    set_seed(training_args.seed)
+        # configure transformers logger
+        # NOTE: if working with distributed training, logging/saving would only
+        # need to be configured on the main or zero process
+        transformers.utils.logging.set_verbosity(training_args.get_process_log_level())
+        transformers.utils.logging.enable_default_handler()
+        transformers.utils.logging.enable_explicit_format()
+        remove_all_file_handlers(transformers.utils.logging.get_logger())
+        add_file_handler(
+            transformers.utils.logging.get_logger(),
+            training_args.get_process_log_level(),
+            os.path.join(training_args.output_dir, "session.log"),
+        )
 
-    # detect last checkpoint if necessary
-    if training_args.do_train and not training_args.overwrite_output_dir:
-        # use upstream function for detection
-        checkpoint = get_last_checkpoint(training_args.output_dir)
+        # check for existing exit code and decide action
+        if (
+            os.path.exists(os.path.join(training_args.output_dir, SUCCESS_FILE))
+            and not training_args.overwrite_output_dir
+            and training_args.do_train
+        ):
+            LOGGER.info(".success file found; therefore training already complete")
+            return None
 
-        # check if checkpoint exists
-        if checkpoint is not None:
-            LOGGER.warning(
-                "Checkpoint detected, resuming training from %s. "
-                "To avoid this behavior, change --output_dir or "
-                "add --overwrite_output_dir to train from scratch" % checkpoint
+        # dump miscellaneous arguments
+        torch.save(
+            {"model_args": model_args, "data_args": data_args},
+            os.path.join(training_args.output_dir, "misc_args.bin"),
+        )
+
+        # log on each process the small summary
+        LOGGER.info(
+            (
+                "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, "
+                "16-bits training: %s"
             )
-    else:
-        checkpoint = None
+            % (
+                training_args.local_rank,
+                training_args.device,
+                training_args.n_gpu,
+                bool(training_args.local_rank != -1),
+                training_args.fp16,
+            )
+        )
 
-    # define new argument based on task name
-    task_dir = os.path.join(data_args.data_dir, data_args.task)
+        # set seed before initializing model
+        set_seed(training_args.seed)
 
-    # load dataset based on task name
-    if data_args.task == "opp_115":
-        data = load_opp_115(task_dir)
-    elif data_args.task == "piextract":
-        data = load_piextract(task_dir)
-    elif data_args.task == "policy_detection":
-        data = load_policy_detection(task_dir)
-    elif data_args.task == "policy_ie_a":
-        data = load_policy_ie_a(task_dir)
-    elif data_args.task == "policy_ie_b":
-        data = load_policy_ie_b(task_dir)
-    elif data_args.task == "policy_qa":
-        data = load_policy_qa(task_dir)
-    elif data_args.task == "privacy_qa":
-        data = load_privacy_qa(task_dir)
+        # detect last checkpoint if necessary
+        if training_args.do_train and not training_args.overwrite_output_dir:
+            # use upstream function for detection
+            checkpoint = get_last_checkpoint(training_args.output_dir)
 
-    print(data)
+            # check if checkpoint exists
+            if checkpoint is not None:
+                LOGGER.warning(
+                    "Checkpoint detected, resuming training from %s. "
+                    "To avoid this behavior, change --output_dir or "
+                    "add --overwrite_output_dir to train from scratch" % checkpoint
+                )
+        else:
+            checkpoint = None
 
-    # NOTE: temporarily raise error
-    raise NotImplementedError
+        # branch into separate workflows depending on task type
+        if data_args.task in [
+            "opp_115",
+            "policy_detection",
+            "policy_ie_a",
+            "privacy_qa",
+        ]:
+            raise NotImplementedError
+        elif data_args.task in ["pi_extract", "policy_ie_b"]:
+            raise NotImplementedError
+        elif data_args.task == "policy_qa":
+            raise NotImplementedError
+
+        # save success file if everything proceeds well
+        save_success_file(training_args.output_dir)
+
+    # cleanup logger regardless of execution workflow (except SIGKILL)
+    finally:
+        remove_all_file_handlers(LOGGER)
 
 
 def main() -> None:
@@ -174,7 +163,7 @@ def main() -> None:
     if data_args.task != "all":
         tasks = [data_args.task]
     else:
-        tasks = TASKS
+        tasks = [task for task in TASKS if task != "all"]
 
     # loop over tasks and seeds
     for task in tasks:
