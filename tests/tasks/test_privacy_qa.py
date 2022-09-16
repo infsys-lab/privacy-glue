1787b0e63072f29ae8ef4b84cb60c1c7f308d786
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-09-16 20:32:20 +0200

    Patch tests for PrivacyQA
    Check for contents of files instead of form

--- tests/tasks/test_privacy_qa.py
+++ tests/tasks/test_privacy_qa.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from tasks.privacy_qa import load_privacy_qa
+import datasets
 import os
 
 
@@ -14,17 +15,33 @@ def test_load_privacy_qa():
     # check that all three splits are included
     assert set(data.keys()) == {"train", "validation", "test"}
 
+    # merge train and validation to train to compare against files
+    data["train"] = datasets.concatenate_datasets([data["train"], data["validation"]])
+    del data["validation"]
+
     # iterate over splits
     for (split, data_split) in data.items():
         # check that all column names are as expected
         assert data_split.column_names == ["question", "text", "label"]
 
-        # ensure all questions are composed of strings and not array-like
-        # objects
-        assert all([isinstance(question, str) for question in data_split["question"]])
+        # define what is expected from the load function
+        expected = set(
+            [
+                (
+                    f"{split} question for PrivacyQA?",
+                    f"{split} answer for PrivacyQA",
+                    f"label-{split}-1",
+                ),
+                (
+                    f"another {split} question for PrivacyQA?",
+                    f"another {split} answer for PrivacyQA",
+                    f"label-{split}-2",
+                ),
+            ]
+        )
 
-        # ensure all text is composed of strings and not array-like objects
-        assert all([isinstance(text, str) for text in data_split["text"]])
-
-        # ensure all labels are composed of strings and not array-like objects
-        assert all([isinstance(label, str) for label in data_split["label"]])
+        # assert that we got what is expected
+        assert (
+            set(zip(data_split["question"], data_split["text"], data_split["label"]))
+            == expected
+        )
