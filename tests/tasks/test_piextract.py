c20ec55b2681d366c31d27b2372cf09178b38817
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-09-17 14:05:04 +0200

    Patch tests for PI-Extract
    1. Check for contents of files instead of form
    2. Use mocking for unit tests and add single integration test to test
       how units work together

--- tests/tasks/test_piextract.py
+++ tests/tasks/test_piextract.py
@@ -1,65 +1,70 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from glob import glob
 from tasks.piextract import load_piextract, merge_ner_tags, read_conll_file
+import datasets
 import pytest
 import os
 
 
+def mocked_conll_output():
+    return {
+        "tokens": [
+            ["train", "check", "for", "PI-Extract"],
+            ["another", "train", "check", "for", "PI-Extract"],
+        ],
+        "ner_tags": [
+            ["O", "O", "O", "label-train-nocollect-1"],
+            ["O", "O", "O", "O", "label-train-nocollect-2"],
+        ],
+    }
+
+
+def mocked_merge_tags_output():
+    return [
+        [
+            ["O", "O", "O", "O"],
+            ["O", "O", "O", "O"],
+            ["O", "O", "O", "O"],
+            [
+                "label-train-nocollect-1",
+                "label-train-nocollect-1",
+                "label-train-nocollect-1",
+                "label-train-nocollect-1",
+            ],
+        ],
+        [
+            ["O", "O", "O", "O"],
+            ["O", "O", "O", "O"],
+            ["O", "O", "O", "O"],
+            ["O", "O", "O", "O"],
+            [
+                "label-train-nocollect-2",
+                "label-train-nocollect-2",
+                "label-train-nocollect-2",
+                "label-train-nocollect-2",
+            ],
+        ],
+    ]
+
+
 def test_read_conll_file():
-    for conll_file in glob(
-        os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "data",
-            "piextract",
-            "**",
-            "*.conll03",
-        )
-    ):
-        # read CONLL file
-        data = read_conll_file(conll_file)
+    conll_file = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "data",
+        "piextract",
+        "CollectUse_false",
+        "train.conll03",
+    )
 
-        # check their keys make sense
-        assert set(data.keys()) == {"tokens", "ner_tags"}
+    # read CONLL file
+    data = read_conll_file(conll_file)
 
-        # check that all tokens are present in list objects
-        assert all(
-            [isinstance(tokens_sample, list) for tokens_sample in data["tokens"]]
-        )
+    # check their keys make sense
+    assert set(data.keys()) == {"tokens", "ner_tags"}
 
-        # check that all tokens are present as strings
-        assert all(
-            [
-                isinstance(token, str)
-                for tokens_sample in data["tokens"]
-                for token in tokens_sample
-            ]
-        )
-
-        # check that all NER tags are present in list objects
-        assert all(
-            [isinstance(ner_tags_sample, list) for ner_tags_sample in data["ner_tags"]]
-        )
-
-        # check that all NER tags have two elements
-        assert all(
-            [
-                isinstance(ner_tag, str)
-                for ner_tag_sample in data["ner_tags"]
-                for ner_tag in ner_tag_sample
-            ]
-        )
-
-        # check that tokens and NER tags have the same length
-        assert all(
-            [
-                len(tokens_sample) == len(ner_tag_sample)
-                for tokens_sample, ner_tag_sample in zip(
-                    data["tokens"], data["ner_tags"]
-                )
-            ]
-        )
+    # assert that we got what is expected
+    assert data == mocked_conll_output()
 
 
 @pytest.mark.parametrize(
@@ -87,6 +92,48 @@ def test_merge_ner_tags(raw_ner_tags, merged_ner_tags):
     assert merge_ner_tags(raw_ner_tags) == merged_ner_tags
 
 
+def test_load_piextract_mocked(mocker):
+    # patch other units in the load_piextract function
+    mocker.patch("tasks.piextract.read_conll_file", return_value=mocked_conll_output())
+    mocker.patch(
+        "tasks.piextract.merge_ner_tags", return_value=mocked_merge_tags_output()
+    )
+
+    # load sample data
+    data = load_piextract(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "piextract")
+    )
+
+    # check that all three splits are included
+    assert set(data.keys()) == {"train", "validation", "test"}
+
+    # merge validation and train to train to compare against files
+    data["train"] = datasets.concatenate_datasets([data["train"], data["validation"]])
+    del data["validation"]
+
+    # iterate over splits
+    for (split, data_split) in data.items():
+        # check that all column names are as expected
+        assert data_split.column_names == ["tokens", "ner_tags"]
+
+        # assert that we got what is expected
+        assert set(
+            zip(
+                map(tuple, data_split["tokens"]),
+                [tuple(map(tuple, ner_tags)) for ner_tags in data_split["ner_tags"]],
+            )
+        ) == set(
+            zip(
+                map(tuple, mocked_conll_output()["tokens"]),
+                [
+                    tuple(map(tuple, ner_tags))
+                    for ner_tags in mocked_merge_tags_output()
+                ],
+            )
+        )
+
+
+@pytest.mark.integration
 def test_load_piextract():
     # load sample data
     data = load_piextract(
@@ -96,49 +143,60 @@ def test_load_piextract():
     # check that all three splits are included
     assert set(data.keys()) == {"train", "validation", "test"}
 
+    # merge validation and train to train to compare against files
+    data["train"] = datasets.concatenate_datasets([data["train"], data["validation"]])
+    del data["validation"]
+
     # iterate over splits
     for (split, data_split) in data.items():
         # check that all column names are as expected
         assert data_split.column_names == ["tokens", "ner_tags"]
 
-        # check that all tokens are present in list objects
-        assert all(
-            [isinstance(tokens_sample, list) for tokens_sample in data_split["tokens"]]
-        )
-
-        # check that all tokens are present as strings
-        assert all(
+        # define what is expected from the load function
+        expected = set(
             [
-                isinstance(token, str)
-                for tokens_sample in data_split["tokens"]
-                for token in tokens_sample
+                (
+                    (f"{split}", "check", "for", "PI-Extract"),
+                    (
+                        ("O", "O", "O", "O"),
+                        ("O", "O", "O", "O"),
+                        ("O", "O", "O", "O"),
+                        (
+                            f"label-{split}-collect-1",
+                            f"label-{split}-nocollect-1",
+                            f"label-{split}-share-1",
+                            f"label-{split}-noshare-1",
+                        ),
+                    ),
+                ),
+                (
+                    ("another", f"{split}", "check", "for", "PI-Extract"),
+                    (
+                        ("O", "O", "O", "O"),
+                        ("O", "O", "O", "O"),
+                        ("O", "O", "O", "O"),
+                        ("O", "O", "O", "O"),
+                        (
+                            f"label-{split}-collect-2",
+                            f"label-{split}-nocollect-2",
+                            f"label-{split}-share-2",
+                            f"label-{split}-noshare-2",
+                        ),
+                    ),
+                ),
             ]
         )
 
-        # check that all NER tags are present in list objects
-        assert all(
-            [
-                isinstance(ner_tags_sample, list)
-                for ner_tags_sample in data_split["ner_tags"]
-            ]
-        )
-
-        # check that all NER tags have two elements
-        assert all(
-            [
-                len(ner_tag) == 4
-                and all([isinstance(inner_tag, str) for inner_tag in ner_tag])
-                for ner_tag_sample in data_split["ner_tags"]
-                for ner_tag in ner_tag_sample
-            ]
-        )
-
-        # check that tokens and NER tags have the same length
-        assert all(
-            [
-                len(tokens_sample) == len(ner_tag_sample)
-                for tokens_sample, ner_tag_sample in zip(
-                    data_split["tokens"], data_split["ner_tags"]
+        # assert that we got what is expected
+        assert (
+            set(
+                zip(
+                    map(tuple, data_split["tokens"]),
+                    [
+                        tuple(map(tuple, ner_tags))
+                        for ner_tags in data_split["ner_tags"]
+                    ],
                 )
-            ]
+            )
+            == expected
         )
