e0454aa316189c26f3dc2ae13cb9dc89c16dbd4b
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-10-12 18:47:21 +0200

    Add comprehensive unit tests
    These cover `privacy_glue.py` and `pipeline_utils.py`

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,65 +7,62 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "model_valid, model_invalid",
-    [
-        ("bert-base-uncased", "bert"),
-        ("sentence-transformers/all-mpnet-base-v2", "all-mpnet"),
-    ],
+    "model_name_or_path",
+    ["bert-base-uncased", "sentence-transformers/all-mpnet-base-v2"],
 )
-def test_ModelArguments(model_valid, model_invalid):
+def test_ModelArguments_valid(model_name_or_path):
     # no error on known models
-    try:
-        ModelArguments(model_name_or_path=model_valid)
-    except AssertionError:
-        pytest.fail("Unexpected assertion encountered")
-
-    # error on unknown models
-    with pytest.raises(Exception) as exception_info:
-        ModelArguments(model_name_or_path=model_invalid)
-    assert exception_info.type == AssertionError
+    ModelArguments(model_name_or_path=model_name_or_path)
 
 
 @pytest.mark.parametrize(
-    "task_valid, task_invalid",
-    [("policy_qa", "policy_extract"), ("privacy_qa", "privacy_ie")],
+    "model_name_or_path",
+    ["bert", "all-mpnet-base-v2"],
 )
-def test_DataArguments_task(task_valid, task_invalid):
-    # no error on known task
-    try:
-        DataArguments(task=task_valid)
-    except AssertionError:
-        pytest.fail("Unexpected assertion encountered")
+def test_ModelArguments_invalid(model_name_or_path):
+    # error on unknown models
+    with pytest.raises(AssertionError):
+        ModelArguments(model_name_or_path=model_name_or_path)
 
+
+@pytest.mark.parametrize(
+    "task",
+    ["privacy_qa", "policy_qa"],
+)
+def test_DataArguments_task_valid(task):
+    # no error on known task
+    DataArguments(task=task)
+
+
+@pytest.mark.parametrize(
+    "task",
+    ["privacy_ie", "policy_ie"],
+)
+def test_DataArguments_task_invalid(task):
     # error on unknown task
-    with pytest.raises(Exception) as exception_info:
-        DataArguments(task=task_invalid)
-    assert exception_info.type == AssertionError
+    with pytest.raises(AssertionError):
+        DataArguments(task=task)
 
 
 def test_DataArguments_data_dir():
     # no error on valid directory
     with tempfile.TemporaryDirectory() as tmp_dir:
-        try:
-            DataArguments(task="all", data_dir=tmp_dir)
-        except AssertionError:
-            pytest.fail("Unexpected assertion encountered")
+        DataArguments(task="all", data_dir=tmp_dir)
 
     # create and delete a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
         pass
 
     # error on invalid directory
-    with pytest.raises(Exception) as exception_info:
+    with pytest.raises(AssertionError):
         DataArguments(task="all", data_dir=tmp_dir)
-    assert exception_info.type == AssertionError
 
 
 def test_get_parser():
     # get parser and check contents
     parser = get_parser()
-    assert set(parser.dataclass_types) == {
-        ModelArguments,
+    assert parser.dataclass_types == [
         DataArguments,
+        ModelArguments,
         TrainingArguments,
-    }
+    ]
