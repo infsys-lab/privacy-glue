79f0ec8e3a544b3d52bc1e505686662f2b8839c7
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-10-12 18:24:59 +0200

    Major bugfix in `policy_qa.py`
    We previously loaded duplicate answers and should not be doing this

--- src/tasks/policy_qa.py
+++ src/tasks/policy_qa.py
@@ -41,17 +41,19 @@ def load_policy_qa(directory: str) -> datasets.DatasetDict:
             title = article["title"]
             for paragraph in article["paragraphs"]:
                 context = paragraph["context"]
-                answers = {}
                 for qa in paragraph["qas"]:
-                    answers["text"] = [answer["text"] for answer in qa["answers"]]
-                    answers["answer_start"] = [
-                        answer["answer_start"] for answer in qa["answers"]
-                    ]
                     temp_dict["id"].append(qa["id"])
                     temp_dict["title"].append(title)
                     temp_dict["context"].append(context)
                     temp_dict["question"].append(qa["question"])
-                    temp_dict["answers"].append(answers)
+                    temp_dict["answers"].append(
+                        {
+                            "text": [answer["text"] for answer in qa["answers"]],
+                            "answer_start": [
+                                answer["answer_start"] for answer in qa["answers"]
+                            ],
+                        }
+                    )
 
         # convert temp_dict to Dataset and insert into DatasetDict
         combined[split] = datasets.Dataset.from_dict(temp_dict)
