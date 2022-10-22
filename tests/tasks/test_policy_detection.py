aa3dd5ce87f67dbcf2a6a8865087e3a9c60d13f4
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-10-22 13:21:47 +0200

    Patch policy detection unit test
    1. Adjust labels from strings to integers
    2. Add additional test to ensure loading function fails if labels are
       not recognized

--- tests/tasks/test_policy_detection.py
+++ tests/tasks/test_policy_detection.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-
 from tasks.policy_detection import load_policy_detection
+import pandas as pd
+import pytest
+import os
 
 
 def test_load_policy_detection():
@@ -18,7 +19,27 @@ def test_load_policy_detection():
     assert sorted(data.keys()) == sorted(["train", "validation", "test"])
 
     # assert that we got what is expected
+    assert data["train"][0] == {"text": "testing twice", "label": 0}
+    assert data["validation"][0] == {"text": "testing once", "label": 1}
+    assert data["test"][0] == {"text": "testing thrice", "label": 1}
 
-    assert data["train"][0] == {"text": "testing twice", "label": "Not Policy"}
-    assert data["validation"][0] == {"text": "testing once", "label": "Policy"}
-    assert data["test"][0] == {"text": "testing thrice", "label": "Policy"}
+
+def test_load_policy_detection_failure(mocker):
+    # mock relevant function
+    mocker.patch(
+        "tasks.policy_detection.pd.read_csv",
+        return_value=pd.DataFrame(
+            {
+                "policy_text": ["some_text_1", "some_text_2", "some_text_3"],
+                "is_policy": ["random_label_1", "random_label_2", "random_label_3"],
+            }
+        ),
+    )
+
+    # load sample data
+    with pytest.raises(ValueError):
+        load_policy_detection(
+            os.path.join(
+                os.path.dirname(os.path.dirname(__file__)), "data", "policy_detection"
+            )
+        )
