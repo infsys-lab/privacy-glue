021bdf7443a18e043119ccea08a938b77ce8131f
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-10-22 13:49:55 +0200

    Update unit tests for PolicyIE-A
    1. Update checks to assert integers instead of strings
    2. Add new test to check that reading fails in case label is not
       recognized

--- tests/tasks/test_policy_ie_a.py
+++ tests/tasks/test_policy_ie_a.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-
 from tasks.policy_ie_a import load_policy_ie_a
+import datasets
+import pytest
+import os
 
 
 def test_load_policy_ie_a():
@@ -23,16 +24,33 @@ def test_load_policy_ie_a():
         # define what is expected from the load function
         expected = sorted(
             [
-                (
-                    f"{split} check for PolicyIE-A",
-                    f"label-{split}-1",
-                ),
-                (
-                    f"another {split} check for PolicyIE-A",
-                    f"label-{split}-2",
-                ),
+                (f"{split} check for PolicyIE-A 1", 0),
+                (f"{split} check for PolicyIE-A 2", 1),
+                (f"{split} check for PolicyIE-A 3", 2),
+                (f"{split} check for PolicyIE-A 4", 3),
+                (f"{split} check for PolicyIE-A 5", 4),
             ]
         )
 
         # assert that we got what is expected
         assert sorted(zip(data_split["text"], data_split["label"])) == expected
+
+
+def test_load_policy_ie_a_failure(mocker):
+    # create mocked dataset dictionary
+    combined = datasets.DatasetDict()
+    for split in ["train", "validation", "test"]:
+        combined["train"] = datasets.Dataset.from_dict(
+            {"text": ["some_text_or_label_1", "some_text_or_label_2"]}
+        )
+
+    # mock relevant function
+    mocker.patch("tasks.policy_ie_a.datasets.load_dataset", return_value=combined)
+
+    # load sample data
+    with pytest.raises(ValueError):
+        load_policy_ie_a(
+            os.path.join(
+                os.path.dirname(os.path.dirname(__file__)), "data", "policy_ie_a"
+            )
+        )
