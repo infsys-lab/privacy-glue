aebd32c2b9b399d1e97a83d0403ed3ce621a1445
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-11-02 15:31:58 +0100

    Update integration tests
    1. Move tests into class structure to re-use parameters
    2. Improve readability of tests
    3. Increase patience to godly value such that training in these
       integration tests always runs to completion

--- tests/test_reproducibility.py
+++ tests/test_reproducibility.py
@@ -26,36 +26,6 @@ require_multi_gpu = pytest.mark.skipif(
 )
 
 
-def get_cli_arguments(task, model_name_or_path, output_dir):
-    return {
-        "--task": task,
-        "--model_name_or_path": model_name_or_path,
-        "--output_dir": output_dir,
-        "--do_train": None,
-        "--do_eval": None,
-        "--do_pred": None,
-        "--max_train_samples": "16",
-        "--max_eval_samples": "16",
-        "--max_predict_samples": "16",
-        "--do_summarize": None,
-        "--load_best_model_at_end": None,
-        "--evaluation_strategy": "epoch",
-        "--save_strategy": "epoch",
-        "--save_total_limit": "2",
-        "--num_train_epochs": "4",
-        "--per_device_train_batch_size": "2",
-        "--per_device_eval_batch_size": "2",
-        "--random_seed_iterations": "1",
-        "--report_to": "none",
-        "--overwrite_output_dir": None,
-        "--overwrite_cache": None,
-    }
-
-
-def convert_dict_to_flat_list(input_dict):
-    return [sub_item for item in input_dict.items() for sub_item in item if sub_item]
-
-
 @pytest.mark.slow
 @pytest.mark.parametrize(
     "task",
@@ -102,74 +72,111 @@ def convert_dict_to_flat_list(input_dict):
         ),
     ],
 )
-def test_reproducibility_across_seeds(
-    task, model_name_or_path, device, runtime, extra_args, tmp_path
-):
-    # initialize CLI arguments
-    args_list_one = (
-        convert_dict_to_flat_list(
-            get_cli_arguments(
-                task, model_name_or_path, os.path.join(str(tmp_path), "run_one")
+class Test_Reproducibility:
+    def get_cli_arguments(self, task, model_name_or_path, output_dir):
+        return {
+            "--task": task,
+            "--model_name_or_path": model_name_or_path,
+            "--output_dir": output_dir,
+            "--do_train": None,
+            "--do_eval": None,
+            "--do_pred": None,
+            "--do_summarize": None,
+            "--max_train_samples": "16",
+            "--max_eval_samples": "16",
+            "--max_predict_samples": "16",
+            "--load_best_model_at_end": None,
+            "--evaluation_strategy": "epoch",
+            "--save_strategy": "epoch",
+            "--save_total_limit": "2",
+            "--num_train_epochs": "4",
+            "--per_device_train_batch_size": "2",
+            "--per_device_eval_batch_size": "2",
+            "--random_seed_iterations": "1",
+            "--report_to": "none",
+            "--overwrite_cache": None,
+            "--early_stopping_patience": str(int(1e5)),
+        }
+
+    def convert_dict_to_flat_list(self, input_dict):
+        return [
+            sub_item for item in input_dict.items() for sub_item in item if sub_item
+        ]
+
+    def test_full_train_reproducibility(
+        self, task, model_name_or_path, device, runtime, extra_args, tmp_path
+    ):
+        # initialize CLI arguments
+        args_list_one = (
+            self.convert_dict_to_flat_list(
+                self.get_cli_arguments(
+                    task, model_name_or_path, os.path.join(str(tmp_path), "run_one")
+                )
             )
+            + extra_args
         )
-        + extra_args
-    )
-    args_list_two = (
-        convert_dict_to_flat_list(
-            get_cli_arguments(
-                task, model_name_or_path, os.path.join(str(tmp_path), "run_two")
+        args_list_two = (
+            self.convert_dict_to_flat_list(
+                self.get_cli_arguments(
+                    task, model_name_or_path, os.path.join(str(tmp_path), "run_two")
+                )
             )
+            + extra_args
         )
-        + extra_args
-    )
 
-    # initialize subprocess env by copying basic environment
-    subprocess_env = os.environ.copy()
+        # initialize subprocess env by copying basic environment
+        subprocess_env = os.environ.copy()
 
-    # conditionally update subprocess environment
-    if device == "cpu":
-        subprocess_env["CUDA_VISIBLE_DEVICES"] = ""
+        # conditionally update subprocess environment
+        if device == "cpu":
+            subprocess_env["CUDA_VISIBLE_DEVICES"] = ""
 
-    # run experiments
-    process_one = subprocess.run(
-        runtime + ["src/privacy_glue.py"] + args_list_one, env=subprocess_env
-    )
-    process_two = subprocess.run(
-        runtime + ["src/privacy_glue.py"] + args_list_two, env=subprocess_env
-    )
+        # run experiments
+        subprocess.run(
+            runtime + ["src/privacy_glue.py"] + args_list_one, env=subprocess_env
+        )
+        subprocess.run(
+            runtime + ["src/privacy_glue.py"] + args_list_two, env=subprocess_env
+        )
 
-    # load dumped benchmark summary one
-    with open(
-        glob(os.path.join(str(tmp_path), "run_one", "*", "benchmark_summary.json"))[0]
-    ) as input_file_stream:
-        benchmark_summary_one = json.load(input_file_stream)
+        # load dumped benchmark summary one
+        with open(
+            glob(os.path.join(str(tmp_path), "run_one", "*", "benchmark_summary.json"))[
+                0
+            ]
+        ) as input_file_stream:
+            benchmark_summary_one = json.load(input_file_stream)
 
-    # load dumped benchmark summary two
-    with open(
-        glob(os.path.join(str(tmp_path), "run_two", "*", "benchmark_summary.json"))[0]
-    ) as input_file_stream:
-        benchmark_summary_two = json.load(input_file_stream)
+        # load dumped benchmark summary two
+        with open(
+            glob(os.path.join(str(tmp_path), "run_two", "*", "benchmark_summary.json"))[
+                0
+            ]
+        ) as input_file_stream:
+            benchmark_summary_two = json.load(input_file_stream)
 
-    # make initial assertions
-    assert process_one.returncode == 0
-    assert process_two.returncode == 0
-    assert benchmark_summary_one == benchmark_summary_two
+        # get latest checkpoint directories
+        checkpoint_one = get_last_checkpoint(
+            glob(
+                os.path.join(str(tmp_path), "run_one", "**", "seed_0"), recursive=True
+            )[0]
+        )
+        checkpoint_two = get_last_checkpoint(
+            glob(
+                os.path.join(str(tmp_path), "run_two", "**", "seed_0"), recursive=True
+            )[0]
+        )
 
-    # get latest checkpoint directories
-    checkpoint_one = get_last_checkpoint(
-        glob(os.path.join(str(tmp_path), "run_one", "**", "seed_0"), recursive=True)[0]
-    )
-    checkpoint_two = get_last_checkpoint(
-        glob(os.path.join(str(tmp_path), "run_two", "**", "seed_0"), recursive=True)[0]
-    )
+        # check weights of final models
+        weights_one = torch.load(os.path.join(checkpoint_one, "pytorch_model.bin"))
+        weights_two = torch.load(os.path.join(checkpoint_two, "pytorch_model.bin"))
 
-    # check weights of final models
-    weights_one = torch.load(os.path.join(checkpoint_one, "pytorch_model.bin"))
-    weights_two = torch.load(os.path.join(checkpoint_two, "pytorch_model.bin"))
+        # assert that benchmark summaries are the same
+        assert benchmark_summary_one == benchmark_summary_two
 
-    # assert that keys are the same
-    assert weights_one.keys() == weights_two.keys()
+        # assert that keys are the same
+        assert weights_one.keys() == weights_two.keys()
 
-    # now assert values of keys are the same
-    for key in weights_one.keys():
-        assert torch.allclose(weights_one[key], weights_two[key])
+        # now assert values of keys are the same
+        for key in weights_one.keys():
+            assert torch.allclose(weights_one[key], weights_two[key])
